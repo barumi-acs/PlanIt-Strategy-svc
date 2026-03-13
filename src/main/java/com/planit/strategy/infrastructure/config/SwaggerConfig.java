df96d99c86e39e59a89de31a56f0b47f0eee96c3
Hyeonbeen Park <hyunbini2942@gmail.com>
2026-03-13 10:40:08 +0900

    feat: Swagger JWT 및 X-User-Id 인증 지원 추가
    - SwaggerConfig: JWT Bearer 토큰 인증 추가
    - SwaggerConfig: X-User-Id 헤더 인증 추가 (내부 서비스용)
    - Swagger UI에서 두 가지 인증 방식 모두 사용 가능

--- src/main/java/com/planit/strategy/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/planit/strategy/infrastructure/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.planit.strategy.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +21,22 @@ public class SwaggerConfig {
                         .version("v1.0")
                         .contact(new Contact()
                                 .name("PlanIt Team")
-                                .email("support@planit.com")));
+                                .email("support@planit.com")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication")
+                        .addList("X-User-Id"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요. 예: eyJhbGc..."))
+                        .addSecuritySchemes("X-User-Id",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-User-Id")
+                                        .description("사용자 ID를 직접 입력 (내부 서비스용). 예: user123")));
     }
 }
